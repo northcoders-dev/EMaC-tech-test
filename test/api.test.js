6654cfd2a381084881b9cd1565b2489e344cc907
nicolalockwood <nicolahelenlockwood@gamil.com>
2022-05-14 11:31:26 +0100

    First test passed- Happy Path 200

--- test/api.test.js
+++ test/api.test.js
@@ -11,20 +11,19 @@ test.only('/api', async () => {
 describe('GET /api/recipes', () => {
 	test.only('200: responds with an array of recipe objects', async () => {
 		const { body } = await request.get('/api/recipes').expect(200);
-		console.log(body);
-		// expect(res.body.recipes).toBeInstanceOf(Array);
-		// res.body.recipes.forEach((recipe) => {
-		// 	expect(recipe).toMatchObject({
-		// 		id: expect.any(Number),
-		// 		imageUrl: expect.any(String),
-		// 		instructions: expect.any(String),
-		// 	});
-		// 	expect(recipe.ingredients).forEach((ingredient) => {
-		// 		expect(ingredient).toiMatchObject({
-		// 			name: expect.any(String),
-		// 			grams: expect.any(Number),
-		// 		});
-		// 	});
-		// });
+		expect(body.recipes).toBeInstanceOf(Array);
+		body.recipes.forEach((recipe) => {
+			expect(recipe).toMatchObject({
+				id: expect.any(String),
+				imageUrl: expect.any(String),
+				instructions: expect.any(String),
+			});
+			recipe.ingredients.forEach((ingredient) => {
+				expect(ingredient).toMatchObject({
+					name: expect.any(String),
+					grams: expect.any(Number),
+				});
+			});
+		});
 	});
 });
