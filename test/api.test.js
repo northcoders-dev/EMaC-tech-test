88e74b1463a605cfcf15f6a5179b1503977bb1b2
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-05-14 16:42:04 +0100

    added recipe endpoint and fetched all recipes

--- test/api.test.js
+++ test/api.test.js
@@ -3,7 +3,43 @@ const server = require('../server');
 
 const request = supertest(server);
 
-test('/api', async () => {
-  const { body } = await request.get('/api').expect(200);
-  expect(body.message).toBe('ok');
+describe('GET /api', () => {
+  describe('200: ok response', () => {
+    test('/api', async () => {
+      const { body } = await request.get('/api').expect(200);
+      expect(body.message).toBe('ok');
+    });
+  });
+});
+
+describe('GET /api/recipes', () => {
+  describe('200: ok response', () => {
+    test('/api/recipes', async () => {
+      const { body } = await request.get('/api/recipes').expect(200);
+    });
+  });
+  describe('200: get list of recipes', () => {
+    test('/api/recipes', async () => {
+      const { body } = await request.get('/api/recipes');
+
+      body.forEach((recipe) => {
+        expect(recipe).toEqual(
+          expect.objectContaining({
+            id: expect.any(String),
+            imageUrl: expect.any(String),
+            instructions: expect.any(String),
+            ingredients: expect.any(Array),
+          })
+        );
+        recipe.ingredients.forEach((ingredient) => {
+          expect(ingredient).toEqual(
+            expect.objectContaining({
+              name: expect.any(String),
+              grams: expect.any(Number),
+            })
+          );
+        });
+      });
+    });
+  });
 });
