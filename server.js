56a26fae64f16463c38b53c0dc6a52dbd81ee633
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-05-15 10:06:18 +0100

    added error handling for wrong path

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const apiRouter = require('./routes/api');
 
 server.use('/api', apiRouter);
 
+server.use((req, res, next) => {
+  res.status(404).send({ msg: 'not found!' });
+});
+
 module.exports = server;
--- test/api.test.js
+++ test/api.test.js
@@ -13,32 +13,42 @@ describe('GET /api', () => {
 });
 
 describe('GET /api/recipes', () => {
-  describe('200: ok response', () => {
-    test('/api/recipes', async () => {
-      const { body } = await request.get('/api/recipes').expect(200);
+  describe('HAPPY PATH', () => {
+    describe('200: ok response', () => {
+      test('/api/recipes', async () => {
+        const { body } = await request.get('/api/recipes').expect(200);
+      });
     });
-  });
-  describe('200: get list of recipes', () => {
-    test('/api/recipes', async () => {
-      const { body } = await request.get('/api/recipes');
+    describe('200: get list of recipes', () => {
+      test('/api/recipes', async () => {
+        const { body } = await request.get('/api/recipes');
 
-      body.forEach((recipe) => {
-        expect(recipe).toEqual(
-          expect.objectContaining({
-            id: expect.any(String),
-            imageUrl: expect.any(String),
-            instructions: expect.any(String),
-            ingredients: expect.any(Array),
-          })
-        );
-        recipe.ingredients.forEach((ingredient) => {
-          expect(ingredient).toEqual(
+        body.forEach((recipe) => {
+          expect(recipe).toEqual(
             expect.objectContaining({
-              name: expect.any(String),
-              grams: expect.any(Number),
+              id: expect.any(String),
+              imageUrl: expect.any(String),
+              instructions: expect.any(String),
+              ingredients: expect.any(Array),
             })
           );
+          recipe.ingredients.forEach((ingredient) => {
+            expect(ingredient).toEqual(
+              expect.objectContaining({
+                name: expect.any(String),
+                grams: expect.any(Number),
+              })
+            );
+          });
         });
+      });
+    });
+  });
+  describe('UNHAPPY PATH', () => {
+    describe('404: path not found', () => {
+      test('/api/wrongpath', async () => {
+        const { body } = await request.get('/api/wrongpath').expect(404);
+        expect(body).toEqual({ msg: 'not found!' });
       });
     });
   });
